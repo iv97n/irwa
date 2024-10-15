07f5a1caee50b40e8dd9597aa74bf3ca7e65c9d3
iv97n <ivan.heg.03@gmail.com>
2024-10-15 16:01:12 +0200

    build: add failed test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,7 +4,7 @@ import unittest
 class TestSample(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(3, 3)  # Test 1
+        self.assertEqual(3, 4)  # Test 1
 
 
 if __name__ == '__main__':
