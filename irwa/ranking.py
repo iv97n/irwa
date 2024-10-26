cd51ca81969c6aa759d530802edcb7db78da3227
martioms01 <marti.oms01@estudiant.upf.edu>
2024-10-26 14:18:26 +0200

    Evaluation de les dues queries obligades

--- irwa/ranking.py
+++ irwa/ranking.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
 import math
+import irwa.preprocessing as ipp
 
 def tf_idf(inverted_index, query, token_tweets):
     N = len(token_tweets)                               # Total number of documents
     scores = defaultdict(float)                         # Store scores for each document
-
+    query = ipp.build_terms(query)
     # For each term in the query
     for term in query:
         if term in inverted_index:
             df = len(inverted_index[term])              # Document Frequency of the term --> en cuántos documentos aparece palabra de la query
-            idf = math.log(N / df)                      # Inverse Document Frequency --> modificación por razones matemáticas del df
+            idf = math.log(N / (1+df))                      # Inverse Document Frequency --> modificación por razones matemáticas del df
             
             for doc_id in inverted_index[term]:         # Para todos los documentos en los que aparece la palabra de la query
                 tf = token_tweets[doc_id].count(term)   # Term Frequency in the document --> contar cuantas veces aparece por documento
