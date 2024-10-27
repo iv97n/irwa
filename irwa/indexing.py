73516af1f8408907e35522e27a1f4f5ce766f3d0
Ivan Hernandez <ivan.hernandez04@estudiant.upf.edu>
2024-10-27 22:28:24 +0100

    refactor: use sets for checking membership in the inverted index

--- irwa/indexing.py
+++ irwa/indexing.py
@@ -2,14 +2,13 @@ from collections import defaultdict
 
 def create_inverted_index(token_tweets):
     
-    inverted_index = defaultdict(list)
+    inverted_index = defaultdict(set)
     
     # Iterate over each document
     for doc_id, tokens in token_tweets.items():
         # For each token in the document
         for token in tokens:
-            # Append the document ID to the token's list
-            if doc_id not in inverted_index[token]:  # Avoid duplicates
-                inverted_index[token].append(doc_id)
+            # Append the document ID to the token's set
+            inverted_index[token].add(doc_id)
     
     return inverted_index
